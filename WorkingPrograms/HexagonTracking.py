b64df1e312f6be110deb868288d43ffd653edba9
abhisar <abhisar.muz@gmail.com>
2020-02-01 19:41:47 -0500

    initial cheez

--- WorkingPrograms/HexagonTracking.py
+++ WorkingPrograms/HexagonTracking.py
@@ -1,5 +1,4 @@
 import math
-import time
 
 import cv2
 import imutils
@@ -11,43 +10,75 @@ def get_angle(a, b, c):
     return ang
 
 
-start_time = time.time()
+usePreviousLocation = False
+f = 0
+color = (0, 0, 255)
 i = 0
 cap = cv2.VideoCapture("/dev/video0")
 # cap = cv2.VideoCapture("/dev/video2")
+last_cnts = []
 while True:
-    _, frame = cap.read()
+    _, yframe = cap.read()
+    frame = cv2.cvtColor(yframe, cv2.COLOR_BGR2HSV)
     frame = cv2.resize(frame, (640, 480))
-    low_green = np.array([0, 150, 20])
-    high_green = np.array([210, 270, 130])
+    low_green = np.array([49, 87, 147])
+    high_green = np.array([85, 223, 255])
     green_mask = cv2.inRange(frame, low_green, high_green)
     green = cv2.bitwise_and(frame, frame, _, mask=green_mask)
     thresh = cv2.threshold(green, 140, 180, cv2.THRESH_BINARY)[1]
     gray = cv2.cvtColor(thresh, cv2.COLOR_BGR2GRAY)
     cnts = cv2.findContours(gray.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
-    threshold_area = 0
+    threshold_area = 10
     (h, w) = frame.shape[:2]
     w1 = w // 2
     h1 = h // 2
-    cv2.circle(frame, (w1, h1), 2, (0, 0, 255), 4)
-    cv2.circle(frame, (w1, h), 2, (0, 0, 255), 4)
-    for cnt in cnts:
-        area = cv2.contourArea(cnt)
-        if area > threshold_area:
-            cv2.drawContours(frame, cnts, -1, (0, 255, 0), 2)
-            rect = cv2.boundingRect(cnt)
-            cv2.contourArea(cnt)
-            assert isinstance(rect, object)
-            x, y, w2, h2 = rect
-            M = cv2.moments(cnt)
-            cX = int(M["m10"] / M["m00"])
-            cY = int(M["m01"] / M["m00"])
-            cv2.rectangle(frame, (x, y), (x + w2, y + h2), (0, 255, 0), 2)
-            cv2.putText(frame, 'Hexagon Detected', (x + w2 + 10, y + h2), 0, 0.3, (0, 255, 0))
-            cv2.circle(frame, (cX, cY), 2, (0, 0, 255), 4)
-            print("Angle: " + str(get_angle((w1, h1), (w1, h), (cX, cY))))
-
+    cv2.circle(frame, (w1, h1), 2, color, 4)
+    cv2.circle(frame, (w1, h), 2, color, 4)
+    if len(cnts) == 1:
+        print("CONTOURS: ", len(cnts))
+        for cnt in cnts:
+            area = cv2.contourArea(cnt)
+            if area > threshold_area:
+                cv2.drawContours(frame, cnts, -1, color, 2)
+                rect = cv2.boundingRect(cnt)
+                cv2.contourArea(cnt)
+                assert isinstance(rect, object)
+                x, y, w2, h2 = rect
+                M = cv2.moments(cnt)
+                cX = int(M["m10"] / M["m00"])
+                cY = int(M["m01"] / M["m00"])
+                cv2.rectangle(frame, (x, y), (x + w2, y + h2), color, 2)
+                cv2.putText(frame, 'Hexagon Detected', (x + w2 + 10, y + h2), 0, 0.3, color)
+                cv2.circle(frame, (cX, cY), 2, color, 4)
+                print("Angle: " + str(get_angle((w1, h1), (w1, h), (cX, cY))))
+                if len(last_cnts) == 1:
+                    last_cnts.pop()
+                else:
+                    pass
+                last_cnts.extend(cnts)
+                f = 0
+    else:
+        if (f < 500):
+            for i in last_cnts:
+                usePreviousLocation = True
+                area = cv2.contourArea(i)
+                if area > threshold_area:
+                    cv2.drawContours(frame, last_cnts, -1, color, 2)
+                    rect1 = cv2.boundingRect(i)
+                    cv2.contourArea(i)
+                    assert isinstance(rect1, object)
+                    x1, y1, w3, h3 = rect1
+                    M = cv2.moments(i)
+                    cX = int(M["m10"] / M["m00"])
+                    cY = int(M["m01"] / M["m00"])
+                    cv2.rectangle(frame, (x1, y1), (x1 + w3, y1 + h3), color, 2)
+                    cv2.putText(frame, 'Hexagon Detected', (x1 + w3 + 10, y1 + h3), 0, 0.3, color)
+                    cv2.circle(frame, (cX, cY), 2, color, 4)
+                    print("Angle: " + str(get_angle((w1, h1), (w1, h), (cX, cY))))
+            f += 1
+        else:
+            pass
     cv2.imshow("Frame", frame)
     key = cv2.waitKey(1)
     if key == 27:
