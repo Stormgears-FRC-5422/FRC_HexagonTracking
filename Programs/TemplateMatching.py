054b32fb8499e651c1fe6fef322e2e52eccce445
abhisar <abhisar.muz@gmail.com>
2020-02-08 11:43:39 -0500

    Gets Distance and can Track the Hexagon

--- Programs/TemplateMatching.py
+++ Programs/TemplateMatching.py
@@ -1,17 +1,21 @@
 import cv2 as cv
 
-img = cv.imread('PicOfHexagon.png', 0)
-img2 = img.copy()
+# img = cv.imread('PicOfHexagon.png', 0)
 template = cv.imread('template.png', 0)
 w, h = template.shape[::-1]
-img = img2.copy()
 # Apply template Matching
-res = cv.matchTemplate(img, template, cv.TM_CCOEFF)
-min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
-# If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
-top_left = max_loc
-bottom_right = (top_left[0] + w, top_left[1] + h)
-cv.rectangle(img, top_left, bottom_right, 255, 2)
+cap = cv.VideoCapture("/dev/video0")
+while True:
+    _, frame = cap.read()
+    img = cv.imread(frame, 0)
+    res = cv.matchTemplate(img, template, cv.TM_CCOEFF)
+    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
+    # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
+    top_left = max_loc
+    bottom_right = (top_left[0] + w, top_left[1] + h)
+    cv.rectangle(img, top_left, bottom_right, 255, 2)
 
-cv.imshow("detected", img)
-cv.waitKey(0)
+    cv.imshow("Frame", img)
+    key = cv.waitKey(1)
+    if key == 27:
+        break
--- Programs/ballTracking.py
+++ Programs/ballTracking.py
@@ -1,23 +1,7 @@
-import math
-
 import cv2
 import imutils
 import numpy as np
 
-
-def get_angle(a, b, c):
-    ang = int(abs(math.degrees(math.atan2(c[1] - b[1], c[0] - b[0]) - math.atan2(a[1] - b[1], a[0] - b[0]))))
-    return ang
-
-
-def distance_calc(height, magic):
-    distance = int(((magic * 56) / height) * 12)
-    return distance
-
-
-def height_calc(centerX, w1, centerY, h):
-    distance = math.sqrt(((centerX - w1) ** 2) + ((centerY - h) ** 2))
-    return distance
 
 
 f = 0
@@ -31,16 +15,16 @@ while True:
     _, yframe = cap.read()
     frame = cv2.cvtColor(yframe, cv2.COLOR_BGR2HSV)
     frame = cv2.resize(frame, (640, 480))
-    low_green = np.array([0, 195, 0])
-    high_green = np.array([40, 255, 255])
-    green_mask = cv2.inRange(frame, low_green, high_green)
-    green = cv2.bitwise_and(frame, frame, _, mask=green_mask)
-    thresh = cv2.threshold(green, 140, 180, cv2.THRESH_BINARY)[1]
+    low_yellow = np.array([20, 100, 110])
+    high_yellow = np.array([30, 255, 255])
+    yellow_mask = cv2.inRange(frame, low_yellow, high_yellow)
+    yellow = cv2.bitwise_and(frame, frame, _, mask=yellow_mask)
+    thresh = cv2.threshold(yellow, 140, 180, cv2.THRESH_BINARY)[1]
     gray = cv2.cvtColor(thresh, cv2.COLOR_BGR2GRAY)
     cnts = cv2.findContours(gray.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
-    min_threshold_area = 10
-    max_threshold_area = 3500
+    min_threshold_area = 0
+    max_threshold_area = 5000
     (h, w) = frame.shape[:2]
 
     w1 = w // 2
@@ -64,20 +48,9 @@ while True:
                 M = cv2.moments(cnt)
                 cX = int(M["m10"] / M["m00"])
                 cY = int(M["m01"] / M["m00"])
-                if calibrateCounter:
-                    calibrateCx = cX
-                    calibrateCy = cY
-                    magic = (height_calc(calibrateCx, calibrateW, calibrateCy, calibrateH) * 20) / 56
-                    print("Magic: ", magic)
-                    calibrateCounter = False
-                cv2.rectangle(frame, (x, y), (x + w2, y + h2), color, 2)
-                cv2.putText(frame, 'Hexagon Detected', (x + w2 + 10, y + h2), 0, 0.3, color)
+                cv2.circle(frame, (x, y), color, 2)
+                cv2.putText(frame, 'Ball Detected', (x + w2 + 10, y + h2), 0, 0.3, color)
                 cv2.circle(frame, (cX, cY), 2, color, 4)
-                print("Angle: " + str(get_angle((w1, h1), (w1, h), (cX, cY))))
-                # print("Distance: ", distance_calc(height_calc(cX, w1, cY, h), magic))
-                print("Distance Surya: ", (height_calc(cX, w1, cY, h) ** 2) * 0.0007763683 - (
-                            0.6344597 * height_calc(cX, w1, cY, h)) + 139.5395)
-                print("Height: ", height_calc(cX, w1, cY, h))
                 if len(last_cnts) == 1:
                     last_cnts.pop()
                 else:
@@ -98,11 +71,9 @@ while True:
                     M = cv2.moments(i)
                     cX = int(M["m10"] / M["m00"])
                     cY = int(M["m01"] / M["m00"])
-                    cv2.rectangle(frame, (x1, y1), (x1 + w3, y1 + h3), color, 2)
+                    cv2.cirlce(frame, (x1, y1), color, 2)
                     cv2.putText(frame, 'Hexagon Detected', (x1 + w3 + 10, y1 + h3), 0, 0.3, color)
                     cv2.circle(frame, (cX, cY), 2, color, 4)
-                    print("Angle: " + str(get_angle((w1, h1), (w1, h), (cX, cY))))
-                    print("Distance: ", distance_calc(height_calc(cX, w1, cY, h), magic))
 
             f += 1
         else:
